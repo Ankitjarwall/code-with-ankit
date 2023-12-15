bb01bb337987c2f623cd9015df2af57440ade267
Ankit Meena <95204345+Ankitjarwall@users.noreply.github.com>
2023-12-15 20:31:57 +0530

    update

---
+++ Python/circle.py
@@ -0,0 +1,8 @@
+def circle(value):
+    result_value = 0
+    number = value*3.14
+    result_value = round(number, 2)
+    print(result_value)
+
+
+circle(1)
---
+++ Python/fizbuzz.py
@@ -0,0 +1,11 @@
+n = 15
+
+for i in range(1, n):
+    if (i % 3 == 0) and (i % 5 == 0):
+        print("FizzBuzz")
+    elif i % 3 == 0:
+        print("Fizz")
+    elif i % 5 == 0:
+        print("Buzz")
+    else:
+        print(i)
